46c99998f607e397c675236b9cfc1048a62bfcd4
payam9 <payamkhosravi4@gmail.com>
2018-05-08 12:49:11 -0700

    orginl files

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hero">
+    <div>
+      <h1 class="display-3">Home</h1>
+      <p class="lead">This is the Homepage of our Portal</p>
+    </div>
+  </div>
+</template>
+
+<style>
+  .hero {
+    height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background-color: #f7f9f9;
+  }
+  .hero .lead {
+    font-weight: 200;
+    font-size: 1.5rem;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import EmployeeInformation from '@/components/EmployeeInformation'
+import Auth from '@okta/okta-vue'
+import PayrollPortal from '@/components/PayrollPortal'
+
+Vue.use(Auth, {
+  issuer: 'https://sjsu-payam.okta.com',
+  client_id: '0oa13uduyfAK6GQ4e2p7',
+  redirect_uri: 'http://73.231.113.29:8080/implicit/callback',
+  scope: 'openid profile email'
+})
+
+Vue.use(Router)
+
+let router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/implicit/callback',
+      component: Auth.handleCallback()
+    },
+    {
+      path: '/EmployeeInformation',
+      name: 'EmployeeInformation',
+      component: EmployeeInformation,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/PayrollPortal',
+      name: 'PayrollPortal',
+      component: PayrollPortal,
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
+})
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+export default router
